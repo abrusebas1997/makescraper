943e33a23f1c75293ba71a3b422147b3e202170d
abrusebas1997 <sebastianabarca@Sebastians-MacBook-Pro.local>
2020-07-03 02:26:30 -0700

    serializing

--- scrape.go
+++ scrape.go
@@ -2,15 +2,18 @@ package main
 
 import (
 	"fmt"
-
+	"io/ioutil"
+	"encoding/json"
 	"github.com/gocolly/colly"
 )
 
-type food struct {
-	Name string `json:"name"`
-	Price int `json:"price"`
-	Rating string `json:"rating"`
+type recipe struct {
 	Image string `json:"image"`
+	Title string `json:"title"`
+	Description int `json:"rating"`
+	Ingredients string `json:"ingredients"`
+	Instructions string `json:"preparation"`
+	Equipment string `json:"Equipment"`
 }
 
 func writeFile(file []byte) {
@@ -19,25 +22,41 @@ func writeFile(file []byte) {
 		panic(err)
 	}
 }
-// main() contains code adapted from example found in Colly's docs:
-// http://go-colly.org/docs/examples/basic/
+func serializeJSON(recipe []recipe) {
+	fmt.Println("Serializing ...")
+	recipeJSON, _ := json.Marshal(recipe)
+	writeFile(recipeJSON)
+	fmt.Println("Done Serializing √")
+	fmt.Println(string(recipeJSON))
+}
+
 func main() {
 	// Instantiate default collector
 	c := colly.NewCollector()
-
+	recipes := []recipe{}
 	// On every a element which has href attribute call callback
-	c.OnHTML("a[href]", func(e *colly.HTMLElement) {
-                link := e.Attr("href")
+	c.OnHTML(".row ol > li", func(e *colly.HTMLElement) {
+
+// Need to figure put how to write all the child texts
+
+		// recipe := recipe{}
+		// recipe.Image = e.Attr("Image")
+		// recipe.Title = e.ChildText("Apple pie")
+		// recipe.Description = e.ChildText("Description")
+		// recipe.Ingredients = e.ChildText("Ingredients")
+		// recipe.Instructions = e.ChildText("Instructions")
+		// recipe.Equipment = e.ChildText("Equipment")
+		// recipes = append(recipes, recipe)
 
 		// Print link
-                fmt.Printf("Link found: %q -> %s\n", e.Text, link)
+		fmt.Printf("Image: %q\n, Title: %q\n, Description: %q\n, Ingredients: %q\n, Instructions: %q\n, Equipment: %q\n", recipe.Image, recipe.Title, recipe.Description, recipe.Ingredients, recipe.Instructions, recipe.Equipment)
+				// fmt.Printf("Link found: %q -> %s\n", e.Text, link)
 	})
-
 	// Before making a request print "Visiting ..."
 	c.OnRequest(func(r *colly.Request) {
 		fmt.Println("Visiting", r.URL.String())
 	})
 
-	// Start scraping on https://hackerspaces.org
-	c.Visit("https://hackerspaces.org/")
+	c.Visit("https://pinchofyum.com/5-ingredient-marinated-tomatoes")
+	serializeJSON(recipes)
 }
