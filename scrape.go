71065fb568e18054c50da3dcd93b174d601d4753
abrusebas1997 <sebastianabarca@Sebastians-MacBook-Pro.local>
2020-07-06 22:59:02 -0700

    finish rerializing

--- scrape.go
+++ scrape.go
@@ -1,19 +1,17 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"io/ioutil"
-	"encoding/json"
 	"github.com/gocolly/colly"
 )
 
-type recipe struct {
-	Image string `json:"image"`
-	Title string `json:"title"`
-	Description int `json:"rating"`
-	Ingredients string `json:"ingredients"`
-	Instructions string `json:"preparation"`
-	Equipment string `json:"Equipment"`
+type quoteWord struct {
+	Text string `json:"text"`
+	Author string `json:"author"`
+	Tags string `json:"tags"`
+
 }
 
 func writeFile(file []byte) {
@@ -22,41 +20,39 @@ func writeFile(file []byte) {
 		panic(err)
 	}
 }
-func serializeJSON(recipe []recipe) {
+func serializeJSON(quote []quoteWord) {
 	fmt.Println("Serializing ...")
-	recipeJSON, _ := json.Marshal(recipe)
-	writeFile(recipeJSON)
+	quotesJSON, _ := json.Marshal(quote)
+	writeFile(quotesJSON)
 	fmt.Println("Done Serializing √")
-	fmt.Println(string(recipeJSON))
+	fmt.Println(string(quotesJSON))
 }
 
 func main() {
 	// Instantiate default collector
 	c := colly.NewCollector()
-	recipes := []recipe{}
+	quotes := []quoteWord{}
+
 	// On every a element which has href attribute call callback
-	c.OnHTML(".row ol > li", func(e *colly.HTMLElement) {
+	c.OnHTML("body > div > div.row.header-box", func(e *colly.HTMLElement) {
 
 // Need to figure put how to write all the child texts
 
-		// recipe := recipe{}
-		// recipe.Image = e.Attr("Image")
-		// recipe.Title = e.ChildText("Apple pie")
-		// recipe.Description = e.ChildText("Description")
-		// recipe.Ingredients = e.ChildText("Ingredients")
-		// recipe.Instructions = e.ChildText("Instructions")
-		// recipe.Equipment = e.ChildText("Equipment")
-		// recipes = append(recipes, recipe)
+		quote := quoteWord{}
+		quote.Text = e.ChildText("text")
+		quote.Author = e.ChildText("author")
+		quote.Tags = e.ChildText("tags")
+		quotes = append(quotes, quote)
 
 		// Print link
-		fmt.Printf("Image: %q\n, Title: %q\n, Description: %q\n, Ingredients: %q\n, Instructions: %q\n, Equipment: %q\n", recipe.Image, recipe.Title, recipe.Description, recipe.Ingredients, recipe.Instructions, recipe.Equipment)
-				// fmt.Printf("Link found: %q -> %s\n", e.Text, link)
+		fmt.Printf("Text: %q\n, Author: %q\n, Tags: %q\n", quote.Text, quote.Author, quote.Tags)
+
+  // Before making a request print "Visiting ..."
 	})
-	// Before making a request print "Visiting ..."
 	c.OnRequest(func(r *colly.Request) {
 		fmt.Println("Visiting", r.URL.String())
 	})
 
-	c.Visit("https://pinchofyum.com/5-ingredient-marinated-tomatoes")
-	serializeJSON(recipes)
+	c.Visit("http://quotes.toscrape.com/")
+	serializeJSON(quotes)
 }
